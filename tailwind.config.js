39bcd49889751ad34bd39b6881c21346c16d82f8
ishii-nobuaki-tech <ishii-nobuaki@sptvjsat.com>
2026-02-20 21:12:16 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./*.{js,ts,jsx,tsx}",           // ルート直下のファイルも含める
+    "./components/**/*.{js,ts,jsx,tsx}" // components 配下も含める
   ],
   theme: {
     extend: {},
